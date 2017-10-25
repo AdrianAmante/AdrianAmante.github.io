caac9ddc7607b374b58284dc96d5f7906bdb2569
Adrian Amante <adrian_amante@oneaffiniti.com>
2017-10-26 01:41:46 +0800

    smoothscroll update

--- oneaffiniti/customscripts.js
+++ oneaffiniti/customscripts.js
@@ -29,7 +29,7 @@ jQuery('a[href*="#"]')
         }, 1000, function() {
           // Callback after animation
           // Must change focus!
-          var $target = $(target);
+          var $target = jQuery(target);
           $target.focus();
           if ($target.is(":focus")) { // Checking if the target was focused
             return false;
