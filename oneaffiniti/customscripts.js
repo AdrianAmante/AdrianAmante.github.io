5146582a8b26e06ef50f7caf50f7c427a71567e0
Adrian Amante <adrian_amante@oneaffiniti.com>
2017-12-16 00:51:11 +0800

    update css&js

--- oneaffiniti/customscripts.js
+++ oneaffiniti/customscripts.js
@@ -65,5 +65,3 @@ jQuery(window).scroll(function() {
 
 jQuery('.author .author-header h1').html(jQuery('.author .author-header h1').html().substring(6));
 jQuery('.author .author-header h1').addClass('blue');
-  
-jQuery('.author .site-main ul li').empty('.byline');
