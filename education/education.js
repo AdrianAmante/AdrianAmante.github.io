2106991627e89700e70556df4ea2373f4e62deac
Adrian Amante <adrian_amante@oneaffiniti.com>
2018-02-21 04:40:43 +0800

    update educ

--- education/education.js
+++ education/education.js
@@ -1,32 +1,34 @@
-function startIntro() {
-    var intro = introJs();
+function startIntro(){
+  var intro = introJs();
     intro.setOptions({
-        steps: [{
-                intro: "Hello world!"
-            },
-            {
-                element: document.querySelector('.step1'),
-                intro: "This is a tooltip."
-            },
-            {
-                element: '.step2',
-                intro: 'More features, more fun.',
-                position: 'left'
-            },
-            {
-                element: '.step3',
-                intro: "Another step.",
-                position: 'bottom'
-            },
-            {
-                element: '.step4',
-                intro: 'Get it, use it.'
-            },
-            {
-                element: '.step5',
-                intro: 'Finally'
-            }
-        ]
+      steps: [
+        { 
+          intro: "Hello world!"
+        },
+        {
+          element: document.querySelector('#step1'),
+          intro: "This is a tooltip."
+        },
+        {
+          element: document.querySelectorAll('#step2')[0],
+          intro: "Ok, wasn't that fun?",
+          position: 'right'
+        },
+        {
+          element: '#step3',
+          intro: 'More features, more fun.',
+          position: 'left'
+        },
+        {
+          element: '#step4',
+          intro: "Another step.",
+          position: 'bottom'
+        },
+        {
+          element: '#step5',
+          intro: 'Get it, use it.'
+        }
+      ]
     });
 
     intro.start();
