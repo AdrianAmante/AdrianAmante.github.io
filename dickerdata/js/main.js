3b5a158b199ad6311677e570430c202c37f8aa41
Adrian Amante <adrian_amante@oneaffiniti.com>
2018-10-02 11:30:12 +0800

    update

--- dickerdata/js/main.js
+++ dickerdata/js/main.js
@@ -1,7 +1,16 @@
 jQuery(window).scroll(function() {
-  if (jQuery(window).scrollTop() > 300) {
+  if (jQuery(window).scrollTop() > 220) {
     jQuery("body").addClass("scrolled");
   } else {
     jQuery("body").removeClass("scrolled");
   }
+});
+
+jQuery(function () {
+
+  jQuery('.resizeFrame').load(function () {
+    var iframeContentWindow = this.contentWindow;
+    var height = iframeContentWindow.jQuery(document).height();
+    this.style.height = height + 'px';
+  });
 });
