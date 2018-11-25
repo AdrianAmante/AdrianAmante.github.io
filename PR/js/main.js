767e447adcd4b279d975deec41afeb4f197b9528
adrianamante <adrian_amante@oneaffiniti.com>
2018-11-25 12:16:55 +0800

    app drawer overlay on mobile instead of push

--- PR/js/main.js
+++ PR/js/main.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
   $("body").bootstrapMaterialDesign();
 });
+
+$(window).on('resize', function () {
+  var win = $(this);
+  if (win.width() < 1200) {
+    $('.bmd-layout-container.bmd-drawer-f-l').removeClass('bmd-drawer-in');
+    $('.bmd-layout-container.bmd-drawer-f-l').addClass('bmd-drawer-overlay');
+    $('.bmd-layout-backdrop').removeClass('in');
+  }
+  else {
+    $('.bmd-layout-container.bmd-drawer-f-l').addClass('bmd-drawer-in');
+    $('.bmd-layout-container.bmd-drawer-f-l').removeClass('bmd-drawer-overlay');
+    $('.bmd-layout-backdrop').removeClass('in');
+  }
+});
