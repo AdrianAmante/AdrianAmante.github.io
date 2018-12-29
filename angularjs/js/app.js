b61c6d17ac5ab06b6ef4ca73f4129567941059e6
adrianamante <adrian_amante@oneaffiniti.com>
2018-12-29 20:48:41 +0800

    added agularJs material bootstrap website starter project

---
+++ angularjs/js/app.js
@@ -0,0 +1,25 @@
+var myApp = angular.module('myApp', [
+    'ngRoute',
+    'myControllers'
+]);
+
+myApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(false);
+    $locationProvider.hashPrefix('');
+    $routeProvider
+        .when('/', {
+            templateUrl: 'js/partials/articles.html',
+            controller: 'ArticleController'
+        })
+        .when('/contact', {
+            templateUrl: 'js/partials/contact.html',
+            controller: 'ContactController'
+        })
+        .when('/details/:itemId', {
+            templateUrl: 'js/partials/details.html',
+            controller: 'DetailsController'
+        });
+
+
+}]);
+  
