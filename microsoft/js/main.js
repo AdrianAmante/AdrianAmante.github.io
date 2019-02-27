14491956f47f2d4b053bbb4486adb969ccc94ef9
Adrian Amante <adrian_amante@oneaffiniti.com>
2019-02-28 05:21:59 +0800

    update site

--- microsoft/js/main.js
+++ microsoft/js/main.js
@@ -1,1 +1,36 @@
-$(document).ready(function () { $('body').bootstrapMaterialDesign(); });
+$(document).ready(function() {
+  $("body").bootstrapMaterialDesign();
+});
+
+//move navigation when scrolled down
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 550) {
+    $(".interested")
+      .addClass("fixed-top")
+      .animate("slow");
+  } else {
+    $(".interested")
+      .removeClass("fixed-top")
+      .animate("fast");
+  }
+
+  if ($(window).scrollTop() > 550) {
+    $(".interested-text p")
+      .addClass("f-size-adj")
+      .animate("slow");
+  } else {
+    $(".interested-text p")
+      .removeClass("f-size-adj")
+      .animate("fast");
+  }
+  
+  if ($(window).scrollTop() > 550) {
+    $(".interested-text h4")
+      .addClass("f-title-adj")
+      .animate("slow");
+  } else {
+    $(".interested-text h4")
+      .removeClass("f-title-adj")
+      .animate("fast");
+  }
+});
