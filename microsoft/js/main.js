94f47f1db5e0e1beea77a3a316506155d4b546ff
Adrian Amante <adrian_amante@oneaffiniti.com>
2019-03-03 15:47:01 +0800

    update page

--- microsoft/js/main.js
+++ microsoft/js/main.js
@@ -4,15 +4,15 @@ $(document).ready(function() {
 
 //move navigation when scrolled down
 $(window).scroll(function() {
-  if ($(window).scrollTop() > 550) {
-    $(".interested")
-      .addClass("fixed-top")
-      .animate("slow");
-  } else {
-    $(".interested")
-      .removeClass("fixed-top")
-      .animate("fast");
-  }
+  // if ($(window).scrollTop() > 550) {
+  //   $(".interested")
+  //     .addClass("fixed-top")
+  //     .animate("slow");
+  // } else {
+  //   $(".interested")
+  //     .removeClass("fixed-top")
+  //     .animate("fast");
+  // }
 
 //   if ($(window).scrollTop() > 550) {
 //     $(".interested-text p")
@@ -24,13 +24,13 @@ $(window).scroll(function() {
 //       .animate("fast");
 //   }
   
-  if ($(window).scrollTop() > 550) {
-    $(".interested-text h4")
-      .addClass("f-title-adj")
-      .animate("slow");
-  } else {
-    $(".interested-text h4")
-      .removeClass("f-title-adj")
-      .animate("fast");
-  }
+  // if ($(window).scrollTop() > 550) {
+  //   $(".interested-text h4")
+  //     .addClass("f-title-adj")
+  //     .animate("slow");
+  // } else {
+  //   $(".interested-text h4")
+  //     .removeClass("f-title-adj")
+  //     .animate("fast");
+  // }
 });
