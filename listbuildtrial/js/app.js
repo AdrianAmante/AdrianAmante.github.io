e3a3d1e35fa0d50e878abec72d89ed86c6bfbc13
Adrian Amante <adrian_amante@oneaffiniti.com>
2019-04-16 10:47:49 +0800

    added listbuild trial

---
+++ listbuildtrial/js/app.js
@@ -0,0 +1,30 @@
+var myApp = angular.module('myApp', [
+    'ngRoute',
+    'ngAnimate',
+    'myControllers'
+]);
+
+myApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(false);
+    $locationProvider.hashPrefix('');
+    $routeProvider
+        .when('/', {
+            templateUrl: 'js/partials/listbuild.html',
+            controller: 'ListbuildController'
+        })
+        .when('/articles', {
+            templateUrl: 'js/partials/articles.html',
+            controller: 'ArticleController'
+        })
+        .when('/contact', {
+            templateUrl: 'js/partials/contact.html',
+            controller: 'ContactController'
+        })
+        .when('/details/:itemId', {
+            templateUrl: 'js/partials/details.html',
+            controller: 'DetailsController'
+        });
+
+
+}]);
+  
